d06f9ff93bf061dc1ef0a07411fd6ebc7ef460a3
Jules Poissonnet <72744048+jpoissonnet@users.noreply.github.com>
2023-04-04 18:01:08 +0200

    feat(navbar): make the navbar responsive (#26)
    * feat(nav): add a navbar
    
    * add a comma
    
    * feat(nav): make the nav responsive
    
    * animation
    
    * link
    
    * fix paths images

--- components/navigation.js
+++ components/navigation.js
@@ -17,30 +17,30 @@ class Navigation extends HTMLElement {
           backdrop-filter: blur(15px);
           box-shadow: 0 2px 3px rgba(0, 0, 0, 0.25);
         }
+        :host * {
+         transition: all 0.3s ease;
+        }
         :host nav{
           display: flex;
-          flex-direction: column;
+          flex-direction: row;
           justify-content: space-between;
           align-items: center;
           position: sticky;
           top: 0;
           left: 0;
-          width: 100%;
-          height: 186px;
-          padding: 20px 0;
+          right: 0;
+          height: 60px;
+          padding: 15px 18px;
           z-index: 1;
         }
+
         :host nav img{
-          width: 150px;
-          height: auto;
+          height: 100%;
         }
-        :host nav ul{
-          display: flex;
-          list-style: none;
-          padding: 0;
-          margin: 0;
-          gap: 20px;
+        :host nav > ul{
+          display: none;
         }
+
         :host nav ul li{
             font-family: Jost, sans-serif;
             text-transform: uppercase;
@@ -49,22 +49,109 @@ class Navigation extends HTMLElement {
             text-decoration: none;
             color: #3E3943
         }
+
+        :host .drawer{
+          position: absolute;
+          content: "";
+          top: 0;
+          left: -100%;
+          width: 75%;
+          height: 100vh;
+          overflow: hidden;
+          background: #ffffff;
+          padding: 20px;
+        }
+        :host .drawer.open{
+          left: 0;
+          box-shadow: 0 0 0 1000px rgba(0, 0, 0, 0.5);
+        }
+
+        :host nav .drawer ul{
+            display: flex;
+            flex-direction: column;
+            padding: 0;
+            margin: 0;
+        }
+
+        :host nav .drawer ul li{
+            list-style: none;
+            letter-spacing: 2px;
+            line-height: 2;
+            border-bottom: 1px solid #3E3943;
+            padding: 10px 0;
+        }
+
+        @media (min-width: 768px) {
+          :host .mobile-link{
+            display: none;
+          }
+          :host nav {
+            flex-direction: column;
+            height: 186px;
+            padding: 20px 0;
+          }
+          :host nav img{
+            width: 150px;
+            height: auto;
+          }
+          :host nav ul{
+            display: flex;
+            list-style: none;
+            padding: 0;
+            margin: 0;
+            gap: 10px;
+          }
+        }
+
       </style>
       <nav>
-        <img src="https://haudrey.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fabc3cfef-58c0-40c1-b855-7d59d6a7925d%2FEP-logo-DEF-carre.png?id=c4206b66-9240-4498-b79d-34fac4501f7d&table=block&spaceId=13fec3ef-4892-4045-a9dd-26c7528efbeb&width=2000&userId=&cache=v2" alt="logo emma pierre" />
+          <button class="mobile-link" id="menu">🍔</button>
+          <img src="https://haudrey.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fabc3cfef-58c0-40c1-b855-7d59d6a7925d%2FEP-logo-DEF-carre.png?id=c4206b66-9240-4498-b79d-34fac4501f7d&table=block&spaceId=13fec3ef-4892-4045-a9dd-26c7528efbeb&width=2000&userId=&cache=v2" alt="logo emma pierre" />
+          <span class="mobile-link">panier</span>
           <ul>
             <li><a href="/index.html">Accueil</a></li>
             <li><a href="/">Précieuses</a></li>
             <li><a href="/">Impertinentes</a></li>
             <li><a href="/">Uniques</a></li>
             <li><a href="/">Par couleur</a></li>
-            <li><a href="pages/about.html">A propos</a></li>
+            <li><a href="/pages/about.html">A propos</a></li>
             <li><a href="/">Blog</a></li>
             <li><a href="/">Contact</a></li>
           </ul>
+          <div class="drawer">
+          <span id="close-drawer">✖</span>
+            <ul>
+                <li><a href="/index.html">Accueil</a></li>
+                <li><a href="/">Précieuses</a></li>
+                <li><a href="/">Impertinentes</a></li>
+                <li><a href="/">Uniques</a></li>
+                <li><a href="/">Par couleur</a></li>
+                <li><a href="/pages/about.html">A propos</a></li>
+                <li><a href="/">Blog</a></li>
+                <li><a href="/">Contact</a></li>
+          </ul>
+          </div>
       </nav>
     `;
     }
+
+    connectedCallback() {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 0) {
+                this.shadowRoot.querySelector('nav').classList.add('scrolled');
+            }
+            if(window.scrollY === 0) {
+                this.shadowRoot.querySelector('nav').classList.remove('scrolled');
+            }
+        });
+        this.shadowRoot.querySelector('#menu').addEventListener('click', () => {
+                this.shadowRoot.querySelector('.drawer').classList.add('open');
+                this.shadowRoot.querySelector('#close-drawer').addEventListener('click', () => {
+                    this.shadowRoot.querySelector('.drawer').classList.remove('open');
+                });
+        });
+    }
+
 }
 
 customElements.define("c-nav", Navigation);

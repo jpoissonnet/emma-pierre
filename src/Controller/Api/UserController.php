a9fbe320d15c8a50f7672ef8c1a682fe8f80cfa3
Cihaan <cihankafadar1@gmail.com>
2023-07-18 14:39:08 +0200

    feat: sign up and login

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -2,44 +2,103 @@
 
 namespace App\Controller\Api;
 
-use App\Controller\AbstractApiController;
+use App\Controller\Api\AbstractApiController;
 use App\Routing\Attribute\Route;
 
 class UserController extends AbstractApiController
 {
-
     #[Route("/api/users", name: "api_users_get")]
     public function getUsers()
     {
-        return json_encode([
-            [
-                'id' => 1,
-                'login' => 'John Doe',
-                'password' => '',
-                'nom' => 'Doe',
-                'prenom' => 'John',
-                'telephone' => '0606060606',
-                'id_role' => 1,
-            ],
-            [
-                'id' => 2,
-                'login' => 'Jane Doe',
-                'password' => '',
-                'nom' => 'Doe',
-                'prenom' => 'Jane',
-                'telephone' => '0606060606',
-                'id_role' => 2,
-            ]
-        ]);
+        try {
+            $this->db->query('SELECT * FROM user');
+            $users = $this->db->fetchAll();
+
+            header('Content-Type: application/json', true, 200);
+            return json_encode($users);
+        } catch (\Throwable $th) {
+            header('Content-Type: application/json', true, 500);
+            return json_encode([
+                'message' => 'Internal error while trying to fetch users'
+            ]);
+        }
+
     }
 
     #[Route("/api/users", name: "api_users_post", httpMethod: "POST")]
     public function createUser()
     {
-       header('Content-Type: application/json', true, 201);
-       return json_encode([
-           'message' => 'User created'
-       ]);
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        $login = $data['login'];
+        $password = $data['password'];
+        $nom = $data['nom'];
+        $prenom = $data['prenom'];
+        $telephone = $data['telephone'];
+
+        $query = $this->db->prepare('SELECT * FROM user WHERE login = :login');
+        $query->execute([
+            'login' => $login
+        ]);
+        $user = $query->fetch();
+
+        if ($user) {
+            header('Content-Type: application/json', true, 409);
+            return json_encode([
+                'message' => 'User already exists'
+            ]);
+        }
+
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        $query = $this->db->prepare('INSERT INTO user (login, password, nom, prenom, telephone, id_role) VALUES (:login, :password, :nom, :prenom, :telephone, :id_role)');
+        $query->execute([
+            'login' => $login,
+            'password' => $hashedPassword,
+            'nom' => $nom,
+            'prenom' => $prenom,
+            'telephone' => $telephone,
+            'id_role' => 2
+        ]);
+
+
+        header('Content-Type: application/json', true, 201);
+        return json_encode([
+            'message' => 'User created'
+        ]);
     }
 
+    #[Route("/api/login", name: "api_login_post", httpMethod: "POST")]
+    public function connect()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        $login = $data['login'];
+        $password = $data['password'];
+
+        $query = $this->db->prepare('SELECT * FROM user WHERE login = :login');
+        $query->execute([
+            'login' => $login
+        ]);
+        $user = $query->fetch();
+
+        if (!$user) {
+            header('Content-Type: application/json', true, 404);
+            return json_encode([
+                'message' => 'User not found'
+            ]);
+        }
+
+        if (!password_verify($password, $user['password'])) {
+            header('Content-Type: application/json', true, 401);
+            return json_encode([
+                'message' => 'Wrong login or password try again nerd'
+            ]);
+        }
+
+        header('Content-Type: application/json', true, 200);
+        return json_encode([
+            'message' => 'User connected'
+        ]);
+    }
 }
